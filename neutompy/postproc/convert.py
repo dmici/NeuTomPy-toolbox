42a2aad2ea84dd7ab5d2e4117ed9d9fe6139a587
dmici <39350703+dmici@users.noreply.github.com>
2018-09-11 08:02:54 +0200

    add postproc module

---
+++ neutompy/postproc/convert.py
@@ -0,0 +1,91 @@
+import numpy as np
+import numexpr as ne
+import logging
+
+__author__  = "Davide Micieli"
+__version__ = "0.1.0"
+__all__     = ['convert_to_uint']
+
+
+logs = logging.getLogger(__name__)
+
+def convert_to_uint(arr, nbit=16, down=None, up=None, crop_roi=None):
+	"""
+	This function converts a floating point array to 8 or 16 bit unsigned integer array.
+	The resampling can be performed in a user-specified dynamic range.
+	The array can be also cropped by specifying the ROI coordinates.
+
+	Parameters
+	----------
+	arr : ndarray
+		The floating point array to convert to unsigned integer array.
+
+	nbit : int
+		The number of bits of the returned array. In must be `8` or `16`, for
+		8-bit or 16-bit unsigned integer, respectively. Default value is `16`.
+
+	down: float, optional
+		The lower range limit. If `None`, the lower limit is computed as the
+		minimum value of the input array.
+
+	up: float, optional
+		The upper range limit. If `None`, the upper limit is computed as the
+		maximum value of the input array.
+
+	crop_roi: tuple, optional
+		Tuple defining the ROI of the array to crop. E.g. for a 3D stack
+		(smin, smax, rmin, rmax, cmin, cmax), while for a 2D image
+		(rmin, rmax, cmin, cmax). Default value is `None`, which disables
+		the crop of the ROI.
+
+	Returns
+	-------
+	m : ndarray
+		The array resampled to 8-bit or 16-bit unsigned integer.
+	"""
+
+	if(arr.ndim != 3 and  arr.ndim != 2):
+		raise ValueError('The input array must have 2 or 3 dimensions.')
+
+	# check number of bits
+	if(nbit == 8):
+		tp  = np.uint8
+	elif(nbit == 16):
+		tp  = np.uint16
+	else:
+		raise ValueError('The output array type must be 8 bit or 16 bit unsigned integer.')
+
+
+	if(crop_roi):
+
+		old_shape = arr.shape
+
+		if(arr.ndim == 3):
+			smin, smax, rmin, rmax, cmin, cmax = crop_roi
+			arr = arr[smin:smax, rmin:rmax, cmin:cmax].astype(np.float32)
+
+		if(arr.ndim == 2):
+			rmin, rmax, cmin, cmax = crop_roi
+			arr = arr[rmin:rmax, cmin:cmax].astype(np.float32)
+
+		logs.debug('Array cropped: original shape = %s, cropped array shape = %s', old_shape, arr.shape)
+
+	arr = arr.astype(np.float32)
+
+	if(down == None):
+		down = arr.min()
+
+	if(up == None):
+		up = arr.max()
+
+	down = down.astype(np.float32)
+	up   = up.astype(np.float32)
+
+	factor = np.float32(2.0**nbit - 1.0)
+
+	arr = np.clip(arr, a_min=down, a_max=up, out=arr)
+	arr = ne.evaluate('factor*(arr - down)/(up - down)', out=arr, truediv=True)
+
+	logs.info('Array converted to %s using the dynamic range %s - %s', tp, down, up)
+
+	return (np.round(arr)).astype(dtype=tp)
---
+++ neutompy/postproc/crop.py
@@ -0,0 +1,106 @@
+import numpy as np
+import numexpr as ne
+
+
+__author__  = "Davide Micieli"
+__version__ = "0.1.0"
+__all__     = ['get_circular_mask', 'circular_crop']
+
+def get_circular_mask(nrow, ncol, radius=None, center=None):
+	"""
+	This function returns a boolean 2D array representing a circular mask.
+	The values outside the circle are `False`, while the inner values are `True`.
+
+	Parameters
+	----------
+	nrow: int
+		The number of rows of the mask array.
+
+	ncol: int
+		The number of columns of the mask array.
+
+	radius: float, optional
+		The radius of the circle in pixel.
+		The default value is 0.5 * min(number of rows,number of columns)
+
+	center: tuple of float, optional (yc, xc)
+		A tuple representing the coordinates of the center of the circle, i.e.: (yc, xc).
+		The default value is the center of the input image.
+
+	Returns
+	-------
+		mask : 2d array
+		A boolean array that represents the circular mask.
+		The values outside the circle are `False`, while the inner values are `True`.
+	"""
+
+
+	if(radius is None):
+		radius = min(ncol, nrow)/2
+
+	if(center is None):
+		yc = ncol/2.0
+		xc = nrow/2.0
+	else:
+		yc, xc = center
+
+	ny = np.arange(ncol)
+	nx = np.arange(nrow)
+
+	x, y = np.meshgrid(nx, ny)
+
+	mask = ( (y-yc + 0.5)**2 + (x-xc + 0.5)**2 ) < (radius)**2
+
+	return mask
+
+
+
+def circular_crop(img, axis=0, radius=None, center=None, cval=0):
+	"""
+	This function performs a circular crop of an image. The values outside the
+	circle are replaced with the value cval (default cval=0).
+
+	Parameters
+	----------
+
+	img: 2d array
+		The array representing the image to crop.
+
+	radius: float, optional
+		The radius of the circle. The default value is 0.5 * min(number of rows,number of columns)
+
+	center: tuple of float, optional (yc, xc)
+		A tuple representing the coordinates of the center of the circle, i.e.: (yc, xc).
+		The default value is the center of the input image.
+
+	cval: float, optional
+		The value used to fill the region outside the circle.
+
+	Returns
+	-------
+
+	out: 2d array
+		The cropped image.
+	"""
+
+	if (img.ndim == 3):
+		img = img.swapaxes(0,axis)
+		nslice, nrow, ncol = img.shape
+	elif (img.ndim == 2):
+		nrow, ncol = img.shape
+	else:
+		raise ValueError("The input array must have 2 or 3 dimensions.")
+
+	# set type of cval
+	cval =	np.array(cval, dtype=img.dtype)
+
+	# get the circular mask	-> True inside the circle
+	mask = get_circular_mask(nrow, ncol, radius, center)
+
+	# apply the mask
+	ne.evaluate('where(mask, img, cval)' , out=img)
+
+	if (img.ndim == 3):
+		img = img.swapaxes(0,axis)
+
+	return img
