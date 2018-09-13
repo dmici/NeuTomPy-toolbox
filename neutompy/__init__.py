6449e68bb1da03447e1972643768927065b7a28f
user <user@X230.homenet.telecomitalia.it>
2018-09-13 10:08:22 +0200

    update __init__.py

--- neutompy/__init__.py
+++ neutompy/__init__.py
@@ -3,8 +3,8 @@ __version__ = '0.1.0'
 from .image.image import *
 from .image.rebin import *
 from .preproc.preproc import *
-# from .recon.recon import *
+from .recon.recon import *
 from .misc.uitools import *
-# from .postproc.convert import *
-# from .postproc.crop import *
-# from .metrics.metrics import *
+from .postproc.convert import *
+from .postproc.crop import *
+from .metrics.metrics import *
