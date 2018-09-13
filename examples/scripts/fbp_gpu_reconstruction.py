18c4f7266ea0d9f65f373884453ed112cf83863d
user <user@X230.homenet.telecomitalia.it>
2018-09-13 07:56:04 +0200

    add example script FBP GPU

---
+++ examples/scripts/fbp_gpu_reconstruction.py
@@ -0,0 +1,46 @@
+"""
+This script performs a complete reconstruction workflow.
+The reconstruction algorithm used is the FBP performed on a GPU.
+"""
+
+import numpy as np
+import neutompy as ntp
+
+
+# set pixel size in cm
+pixel_size  = 0.0029
+
+# set the last angle value of the CT scan: np.pi or 2*np.pi
+last_angle = 2*np.pi
+
+# read dataset containg projection, dark-field, flat-field images and the projection at 180 degree
+proj, dark, flat, proj_180 = ntp.read_dataset()
+
+# normalize the projections to dark-field, flat-field images and neutron dose
+norm, norm_180 = ntp.normalize_proj(proj, dark, flat, proj_180=proj_180, dose_draw=True, crop_draw=True)
+
+# rotation axis tilt correction
+norm = mt.correction_COR(norm, norm[0], norm_180)
+
+# clean up memory
+del dark; del flat; del proj; del proj_180
+
+# remove outliers
+norm = ntp.remove_outliers_stack(norm, 1, 0.1, outliers='dark', out=norm)
+norm = ntp.remove_outliers_stack(norm, 1, 0.08, outliers='bright', out=norm)
+
+# perform minus-log transform
+norm =  ntp.log_transform(norm, out=norm)
+
+# FBP reconstruction using GPU
+print('> Reconstruction...')
+angles = np.linspace(0, last_angle, norm.shape[0], endpoint=False)
+rec    = ntp.reconstruct(norm, angles, 'FBP_CUDA', pixel_size=pixel_size)
+
+
+# select the directory and the prefix file name of the reconstructed images to save.
+recon_dir = mt.save_filename_gui('',
+                    message = 'Select the folder and the prefix name for the reconstructed images...')
+
+print('> Writing reconstructed slices...')
+mt.write_tiff_stack(recon_dir, rec)
