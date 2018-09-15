f3533fb9136cba5ca337ed1936bbe5dfe4ba0279
dmici <39350703+dmici@users.noreply.github.com>
2018-09-15 19:12:25 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['IPython.sphinxext.ipython_directive',
-    'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
