2c2fe201ad3b7553d08d11936e6c52bceadd1cb7
dmici <39350703+dmici@users.noreply.github.com>
2018-09-19 02:27:03 +0200

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neutompy",
-    version="1.0.0",
+    version="1.0.1",
     author="Davide Micieli",
     author_email="neutompy@gmail.com",
     description="Python package for tomographic data processing and reconstruction",
